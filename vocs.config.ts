e0e4b978dd58fe7abb017342f3a3b6e08dbb1ee6
Sweets Sweetman <sweetmantech@gmail.com>
2025-04-27 14:24:23 -0300

    examples

--- vocs.config.ts
+++ vocs.config.ts
@@ -12,6 +12,27 @@ export default defineConfig({
       link: "/environment-setup",
     },
     {
+      text: "Examples",
+      items: [
+        {
+          text: "Create an EVM Account",
+          link: "/create-evm-account",
+        },
+        {
+          text: "Fund an EVM Account",
+          link: "/fund-evm-account",
+        },
+        {
+          text: "Create an EVM Smart Account",
+          link: "/create-smart-account",
+        },
+        {
+          text: "General Transactions",
+          link: "/general-transactions",
+        },
+      ],
+    },
+    {
       text: "Example",
       link: "/example",
     },
